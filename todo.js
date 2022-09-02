ad648608a4a8a149bc7d2b0f78e3a05cae9754ba
Fernanda Liz <fernandaliz2004@hotmail.com>
2022-09-02 15:48:37 -0300

    adiciona botao de deletar

--- todo.js
+++ todo.js
@@ -3,6 +3,9 @@ var app = new Vue({
     data: {
         message: 'Ola Vue',
         tasks: [],
+        titulo: null,
+        dataDia: null,
+        projeto: null,
     },
     methods: {
         getTasks() {
@@ -18,7 +21,30 @@ var app = new Vue({
             span.onclick = function () {
                 document.querySelector('.modalNovaTask').style.display = 'none'
             }
-        }
+        },
+        async addTasks() {
+            const addNoBanco = await fetch("http://localhost:3000/tasks", {
+                method: 'POST',
+                headers: { 'Content-Type': "application/json" },
+                body: JSON.stringify({ title: this.titulo, project: this.projeto, dueTo: this.dataDia })
+            })
+            const recieve = await addNoBanco.json()
+            console.log(recieve)
+        },
+        async deletar(id) {
+            const deletaTask = await fetch(`http://localhost:3000/tasks/${id}`, {
+                method: 'DELETE',
+            })
+            const recieve = await deletaTask.json()
+            console.log(recieve)
+        },
+        // editar(id) {
+        //     const editaTask = await fetch(`http://localhost:3000/tasks/${id}`, {
+        //         method: 'PATCH',
+        //     })
+        //     const recieve = await editaTask.json()
+        //     console.log(recieve)
+        // },
     },
     created() {
         this.getTasks();
